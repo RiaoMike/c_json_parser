e2b045468e2cd983c41203642520c861fcc713ec
RiaoMike <xuruimaths@gmail.com>
2024-04-24 17:00:27 +0800

    parse number

--- leptjson.c
+++ leptjson.c
@@ -1,11 +1,18 @@
-#include <stdlib.h>
-#include <assert.h>
+#include <stdlib.h> /* strtod */
+#include <assert.h> /* assert */
+#include <errno.h> /* errno */
+#include <math.h> /* HUGE_VAL */
 #include "leptjson.h"
 
 
 lept_type get_lept_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
+}
+
+double get_lept_number(const lept_value* v) {
+    assert(v != NULL && v->type == LEPT_NUMBER);
+    return v->n;
 }
 
 // !!注意下面代码是错误的，野指针，即使经常写代码也容易犯这种错误
@@ -32,8 +39,10 @@ int lept_parse(lept_value* v, const char* json) {
     int ret;
     if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
         lept_parse_whitespace(&c);
-        if (*c.json != '\0')
+        if (*c.json != '\0') {
+            v->type = LEPT_NULL;
             ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
     }
     return ret;
 }
@@ -44,7 +53,7 @@ static int lept_parse_value(lept_content* c, lept_value* v) {
         case 't': return lept_parse_literal(c, v, "true", LEPT_TRUE);
         case 'f': return lept_parse_literal(c, v, "false", LEPT_FALSE);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default: return LEPT_PARSE_INVALID_VALUE;
+        default: return lept_parse_number(c, v);
     }
 }
 
@@ -58,6 +67,45 @@ static int lept_parse_literal(lept_content* c, lept_value* v, const char* litera
     }
     c->json += index;
     v->type = type;
+    return LEPT_PARSE_OK;
+}
+
+static double lept_parse_number(lept_content* c, lept_value* v) {
+    const char* p = c->json;
+    // check format
+    // 负号
+    if (*p == '-') 
+        p++;
+    // 数字
+    if (*p == '0') p++;
+    else {
+        if (!ISDIGIT0TO9(*p)) return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    // 小数
+    if (*p == '.') {
+        p++;
+        // 小数后面一个以上数字
+        if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    // 指数
+    if (*p == 'e' || *p == 'E') {
+        p++;
+        // 指数可能有正负
+        if (*p == '-' || *p == '+') p++;
+        // 后面是一个以上数字
+        if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+
+    // 处理越界
+    errno = 0;
+    v->n = strtod(c->json, NULL);
+    if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+        return LEPT_PARSE_NUMBER_TOO_BIG;
+    c->json = p;
+    v->type = LEPT_NUMBER;
     return LEPT_PARSE_OK;
 }
 
--- leptjson.h
+++ leptjson.h
@@ -4,7 +4,11 @@
 #include <assert.h>
 
 // 用于判断json类型是否是期望类型
-#define EXPECT(c, ch) do { assert(*c->json == ch); } while (0)
+#define EXPECT(c, ch) do { assert(*c->json == (ch)); } while (0)
+
+// 用于判断json数字类型是否合法
+#define ISDIGIT(ch) ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT0TO9(ch) ((ch) >= '1' && (ch) <= '9')
 
 // 表示json值类型
 typedef enum { LEPT_NULL, LEPT_TRUE, LEPT_FALSE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT} lept_type;
@@ -16,6 +20,7 @@ typedef struct {
 
 // json解析树节点
 typedef struct {
+    double n; // useful only when type --> LEPT_NUMBER
     lept_type type;
 } lept_value;
 
@@ -25,7 +30,8 @@ enum {
     LEPT_PARSE_OK = 0, // 解析成功
     LEPT_PARSE_EXPECT_VALUE, // 全部是空格
     LEPT_PARSE_INVALID_VALUE, // 非法值
-    LEPT_PARSE_ROOT_NOT_SINGULAR // 空格后面还有值
+    LEPT_PARSE_ROOT_NOT_SINGULAR, // 空格后面还有值
+    LEPT_PARSE_NUMBER_TOO_BIG // 解析数字太大
 };
 
 // 解析json串开头的空白字符，包括空白，制表，换行，回车
@@ -35,6 +41,9 @@ static void lept_parse_whitespace(lept_content* c);
 // literal 为 "null", "true", "false"
 static int lept_parse_literal(lept_content* c, lept_value* v, const char* literal, lept_type type);
 
+// 解析json数字类型
+static double lept_parse_number(lept_content* c, lept_value* v);
+
 // 解析json串，被主函数lept_parse调用
 static int lept_parse_value(lept_content* c, lept_value* v);
 
@@ -43,5 +52,8 @@ int lept_parse(lept_value* v, const char* json);
 
 // 获取节点中json值类型
 lept_type get_lept_type(const lept_value* v);
+
+// 获取数字节点上的值
+double get_lept_number(const lept_value* v);
 
 #endif
